efa4cec796f01e2f7e58d8bb610d68b6030b51b1
nashamri <designernasser@gmail.com>
2025-03-13 23:02:44 +0300

    testing how perms work in django

--- journal_homepage/urls.py
+++ journal_homepage/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
- 
+    path('author_home/', views.author_home, name='author_home'),
+    path('reviewer_home/', views.reviewer_home, name='reviewer_home'),
 
 ]
--- journal_homepage/views.py
+++ journal_homepage/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import permission_required
   
 def homepage(request):
-    return render(request, 'journal_homepage/homepage.html', {'message': 'Welcome to our Journal'})
+    return render(request, 'journal_homepage/homepage.html')
+
+@permission_required('sessions.author_perm')
+def author_home(request):
+    return render(request, 'journal_homepage/author_home.html')
+
+@permission_required('sessions.reviewer_perm')
+def reviewer_home(request):
+    return render(request, 'journal_homepage/reviewer_home.html')
+
