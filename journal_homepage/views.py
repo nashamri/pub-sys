94aa8f011ce39f420ece441cd65153533241f8fb
nashamri <designernasser@gmail.com>
2025-03-19 00:08:48 +0300

    testing users groups and their permissions

--- journal_homepage/views.py
+++ journal_homepage/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test, login_required
 
-  
+def group_required(group_name):
+    def check_group(user):
+        return user.groups.filter(name=group_name).exists()
+    return user_passes_test(check_group)
+
+
 def homepage(request):
-    return render(request, 'journal_homepage/homepage.html', {'message': 'Welcome to our Journal'})
+    context = {
+        'is_author': request.user.groups.filter(name='author').exists(),
+        'is_reviewer': request.user.groups.filter(name='reviewer').exists(),
+    }
+    return render(request, 'journal_homepage/homepage.html', context)
+
+@login_required
+@group_required('author')
+def author_home(request):
+    return render(request, 'journal_homepage/author_home.html')
+
+@login_required
+@group_required('reviewer')
+def reviewer_home(request):
+    return render(request, 'journal_homepage/reviewer_home.html')
+
+
