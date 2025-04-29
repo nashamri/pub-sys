0166088275f9414a8f33494da4189db7b79ad9c3
nashamri <designernasser@gmail.com>
2025-04-29 23:31:49 +0300

    allow host name

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@837%vbng-5&%kaljl4fcl4^z2!b51#@4zf=^e#wu911gdjv4)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "nashamri.pythonanywhere.com" ]
 
 
 # Application definition
